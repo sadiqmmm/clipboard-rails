c2e853dd148a295f539e59bb0f8ea90c3ab0b0ef
Mohammed Sadiq <sadiqmmm@gmail.com>
2015-09-30 17:17:32 +0530

    Intial commit

---
+++ lib/clipboard/rails.rb
@@ -0,0 +1,7 @@
+require "clipboard/rails/version"
+
+module Clipboard
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/clipboard/rails/version.rb
@@ -0,0 +1,5 @@
+module Clipboard
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
